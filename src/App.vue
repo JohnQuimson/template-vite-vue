f55751cb05235713ef202ab817ef6f89a62cbffa
JohnQuimson <john.quimson777@gmail.com>
2023-12-14 14:18:44 +0100

    add: store.js

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,21 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import { store } from './store.js';
 
 export default {
   name: 'App',
+
   components: {
     AppHeader,
     AppMain,
     AppFooter,
+  },
+
+  data() {
+    return {
+      store,
+    };
   },
 };
 </script>
