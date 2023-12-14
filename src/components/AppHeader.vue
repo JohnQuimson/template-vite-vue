f14804492310677c19bf50e8b61167460265fde8
JohnQuimson <john.quimson777@gmail.com>
2023-12-14 13:58:18 +0100

    sass installed

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template></template>
+
+<style scoped lang="scss">
+@use '../assets/scss/main.scss' as *;
+</style>
