8f4c27c9c52d8df4aa0b44b2cb3b7b13d25add9d
JohnQuimson <john.quimson777@gmail.com>
2024-05-08 21:36:19 +0100

    feat: scaffolding

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,22 @@
 <script>
+import Jumbotron from './Jumbotron.vue';
+import Slider from './Carousel.vue';
+
 export default {
   name: 'Main',
-  components: {},
+
+  components: {
+    Jumbotron,
+    Slider,
+  },
 };
 </script>
 
-<template></template>
+<template>
+  <main>
+    <Jumbotron />
+    <Slider />
+  </main>
+</template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/Carousel.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: 'Carousel',
+  data() {
+    return {
+      technologies: ['bootstrap.png', 'css.png', 'html.png'],
+    };
+  },
+};
+</script>
+
+<template>
+  <section>
+    <img src="../../public/icons/javascript.png" alt="img" />
+  </section>
+</template>
+
+<style scoped lang="scss">
+@use '../assets/scss/style.scss' as *;
+</style>
